6e4b9ab1f73c1dd14791f0b9a164910993e0fac8
thatguyKacper <kacper.m.wojtowicz@gmail.com>
2023-09-22 13:28:52 +0200

    initial setup

---
+++ client/astro.config.mjs
@@ -0,0 +1,32 @@
+import { loadEnv } from "vite";
+const {
+  PUBLIC_SANITY_STUDIO_PROJECT_ID,
+  PUBLIC_SANITY_STUDIO_DATASET,
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_DATASET,
+} = loadEnv(import.meta.env.MODE, process.cwd(), "");
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+// Different environments use different variables
+const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
+const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "hybrid",
+  adapter: vercel(),
+  integrations: [sanity({
+    projectId,
+    dataset,
+    studioBasePath: "/admin",
+    useCdn: false,
+    // `false` if you want to ensure fresh data
+    apiVersion: "2023-09-22" // Set to date of setup to use the latest API version
+  }),tailwind(), react()]
+});
